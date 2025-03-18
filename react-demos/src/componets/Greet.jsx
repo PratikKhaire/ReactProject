9590bb3cf12346cffa7452a5302374cf69c25be2
pratik <pratik.shekhar.khaire@gmail.com>
2025-03-18 23:55:17 +0530

    Initial commit

---
+++ react-demos/src/componets/Greet.jsx
@@ -0,0 +1,5 @@
+const Greet = () => {
+  return <h1>welcome form the greet </h1>;
+};
+
+export default Greet;
---
+++ react-demos/src/componets/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+const WelcomeMessage = () => {
+  return (
+    <div>
+      <h2>Hello World </h2>
+      <p>Welcome to learning jsx </p>
+    </div>
+  );
+};
+
+export default WelcomeMessage;
