9deeb12d16e05018bbd4fcd36b2462056f167ace
Pratik Khaire <Pratik.shekhar.khaire@gmail.com>
2025-08-15 10:32:43 +0530

    Removed all files except myprofile

--- react-demos/src/componets/Footer.jsx
+++
@@ -1,7 +0,0 @@
-const Footer = ()  => {
-    return <footer>
-        <p> @ 2024 my website </p>
-    </footer>
-}
-
-export default Footer;
--- react-demos/src/componets/Greet.jsx
+++
@@ -1,6 +0,0 @@
-const Greet = () => {
-  return <h1>welcome form the greet </h1>;
-};
-
-
-export default Greet;
